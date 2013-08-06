24819e95b4a78fbf951febb6a104279fb2fe2dfc
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-06 22:38:36 +0400

    [feature] api for colleges

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var mwcCore = require('mwc_kernel'),
   express = require('express'),
   path = require('path');
 
-
 //setting up the config
 var MWC = mwcCore({
-  'hostUrl':'http://vvv.msk0.ru/',//'http://mwcwelcome.herokuapp.com/',//todo - change it
+//'mongoUrl':"mongodb://localhost/mwcdataentry",
+  'hostUrl':'http://vvv.msk0.ru/',//'http://mwcwelcome.herokuapp.com/',//todo - change it to your site!
   'secret': ((process.env.secret)?(process.env.secret):'lAAAAalalala1')
 });
 
@@ -13,7 +13,7 @@ MWC.extendApp(function(core){
   core.app.locals.delimiters = '[[ ]]';
 });
 MWC.usePlugin(require('mwc_plugin_hogan_express'));
-
+MWC.extendModel('Colleges',require('./models/colleges.js'));
 MWC.extendMiddlewares(function(core){
   return express.static(path.join(__dirname, 'public'));
 });
@@ -28,10 +28,16 @@ MWC.extendRoutes(function (core) {
       response.render('auth');
     }
   });
+
+  core.app.get('/api/colleges',function(request,response){
+    if(request.user){
+      request.model.Colleges.find({},function(err,colleges){
+        response.json(colleges);
+      });
+    } else {
+      response.send(401);
+    }
+  });
 });
 MWC.listen();
-
-MWC.on('notify',function(message){
-  console.log(message);
-});
 
