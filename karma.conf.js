4b22fd056cd1fb06cfeea12b69e98e404e6e72ff
Alexander Petrovich <chopachom@gmail.com>
2013-08-16 21:48:44 +0400

    tests(karma): moved tests inside public folder

--- karma.conf.js
+++ karma.conf.js
@@ -11,8 +11,8 @@ files = [
   'app/bower_components/angular-mocks/angular-mocks.js',
   'app/scripts/*.js',
   'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
+  'public/test/mock/**/*.js',
+  'pubic/test/spec/**/*.js'
 ];
 
 // list of files to exclude
