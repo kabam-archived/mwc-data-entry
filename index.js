2f5a5a8de27e48cad4d22c7c395f84b1575d84fe
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-08-19 16:21:51 +0400

    [fix] service starts but do now work.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var mwcCore = require('mwc_kernel'),
+var mwcCore = require('kabam-kernel'),
   express = require('express'),
+  url = require('url'),
   path = require('path');
 
 //setting up the config
@@ -12,7 +13,7 @@ var MWC = mwcCore({
 MWC.extendApp(function (core) {
   core.app.locals.delimiters = '[[ ]]';
 });
-MWC.usePlugin(require('mwc_plugin_hogan_express'));
+MWC.usePlugin(require('kabam-plugin-hogan'));
 
 var protectorMiddlewareFunction = function (mwc) {
   return function (request, response, next) {
@@ -33,7 +34,7 @@ var protectorMiddlewareFunction = function (mwc) {
 MWC.extendMiddleware(['development', 'staging', 'production'], '/api', protectorMiddlewareFunction);
 MWC.extendMiddleware(['development', 'staging', 'production'], '/data-entry', protectorMiddlewareFunction);
 
-MWC.usePlugin(require('mwc_plugin_rest'));
+MWC.usePlugin(require('kabam-plugin-rest'));
 MWC.extendModel('Colleges', require('./models/colleges.js'));
 MWC.extendMiddleware(function (mwc) {
   return express.static(path.join(__dirname, 'public'));
@@ -45,20 +46,20 @@ MWC.extendMiddleware(function (mwc) {
 
 //TODO - see readme - https://github.com/mykabam/kabam-kernel/blob/master/README.md#introduction
 
-  mwc.extendMiddleware('production',function(core){
+MWC.extendMiddleware('production',function(core){
     return express.static(path.join(__dirname, 'public'));
   });
-  mwc.extendMiddleware('production',function(core){
+MWC.extendMiddleware('production',function(core){
     return express.static(path.join(__dirname, '.tmp'));
   });
 
-  mwc.extendMiddleware('production',function(core){
+MWC.extendMiddleware('production',function(core){
     return express.static(path.join(__dirname, 'dist/public'));
   });
 
 
 //TODO: should be in core? //no, not in this wau
-mwc.extendMiddleware(function(core){
+MWC.extendMiddleware(function(core){
   return function(req, res, next) {
     // only return XSRF-TOKEN for index page
     if(url.parse(req.url).pathname === '/') {
