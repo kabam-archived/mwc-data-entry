f950df855f8e5dead7882f94bf27d526d9546266
Alexander Petrovich <chopachom@gmail.com>
2013-08-28 16:00:41 +0400

    chore: Working password login/signup

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-var mwcCore = require('kabam-kernel'),
+var MWCKernel = require('kabam-kernel'),
   express = require('express'),
   url = require('url'),
-  path = require('path');
+  path = require('path'),
+  ENV = process.env.NODE_ENV || 'development',
+  config = require('./config/config.json')[ENV];
 
 //setting up the config
-var MWC = mwcCore({
-  'mongoUrl': "mongodb://localhost/mwc_dev",
-  'hostUrl': 'http://vvv.msk0.ru/',//'http://mwcwelcome.herokuapp.com/',//todo - change it to your site!
-  'secret': ((process.env.secret) ? (process.env.secret) : 'lAAAAalalala1')
-});
+var MWC = MWCKernel(config);
 
 MWC.extendApp(function (core) {
   core.app.locals.delimiters = '[[ ]]';
 });
+
 MWC.usePlugin(require('kabam-plugin-hogan'));
 
 var protectorMiddlewareFunction = function (mwc) {
@@ -29,64 +28,41 @@ var protectorMiddlewareFunction = function (mwc) {
       next();
     }
   };
-}
+};
 
 MWC.extendMiddleware(['development', 'staging', 'production'], '/api', protectorMiddlewareFunction);
-MWC.extendMiddleware(['development', 'staging', 'production'], '/data-entry', protectorMiddlewareFunction);
+//MWC.extendMiddleware(['development', 'staging', 'production'], '/data-entry', protectorMiddlewareFunction);
 
 MWC.usePlugin(require('kabam-plugin-rest'));
 MWC.extendModel('Colleges', require('./models/colleges.js'));
-MWC.extendMiddleware(function (mwc) {
+
+
+MWC.extendMiddleware('development', function (core) {
   return express.static(path.join(__dirname, 'public'));
 });
-
-// this should be defined in a grunt task when we will integrate mwc with grunt
-// NO, this have to be done by publish asserts command
-
-
-//TODO - see readme - https://github.com/mykabam/kabam-kernel/blob/master/README.md#introduction
-
-MWC.extendMiddleware('production',function(core){
-    return express.static(path.join(__dirname, 'public'));
-  });
-MWC.extendMiddleware('production',function(core){
-    return express.static(path.join(__dirname, '.tmp'));
-  });
-
-MWC.extendMiddleware('production',function(core){
-    return express.static(path.join(__dirname, 'dist/public'));
-  });
-
-
-//TODO: should be in core? //no, not in this wau
-MWC.extendMiddleware(function(core){
-  return function(req, res, next) {
-    // only return XSRF-TOKEN for index page
-    if(url.parse(req.url).pathname === '/') {
-      res.cookie('XSRF-TOKEN', req.session._csrf);
-    }
-    next();
-  }
+MWC.extendMiddleware('development', function (core) {
+  return express.static(path.join(__dirname, '.tmp'));
 });
+
+MWC.extendMiddleware('production', function (core) {
+  return express.static(path.join(__dirname, 'dist/public'));
+});
+
 
 
 MWC.extendRoutes(function (core) {
-
-  core.app.get('/', function (request, response) {
-    if (request.user) {
-      response.render('logined');
-    } else {
-      response.status(403);
-      response.render('auth', {'title': "MWC-Data-Entry"});
+  core.app.get('/*', function (request, response, next) {
+    //TODO: any methods on a user to get a frontend-friendly profile?
+    var user = {};
+    if(request.user){
+      user = {
+        'username':request.user.username,
+        'email':request.user.email
+      }
     }
-  });
-
-  core.app.get('/data-entry', function (request, response) {
-    if (request.user) {
-      response.send('It will be a single page application seed page for data entry');
-    } else {
-      response.send(401);
-    }
+    response.render('index', {
+      user: JSON.stringify(user).replace('</', '<\/')
+    });
   });
 });
 
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
-angular.module('mwcDataEntryApp', [])
-  .config(function ($routeProvider) {
+angular.module('mwcDataEntryApp', ['CoreAuth.Services', 'CoreAuth.Controllers'])
+  .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+      .when('/', {templateUrl:'/views/main.html', controller: 'MainCtrl'})
+      .when('/login', {templateUrl: '/views/login.html', controller: 'GenericLoginCtrl'})
+      .when('/signup', {templateUrl: '/views/signup.html', controller: 'GenericSignupCtrl', loginRequired: false})
+      .otherwise({redirectTo: '/'})
+  }])
+  .run(['guard', function(guard){
+    guard.watch();
+  }]);
